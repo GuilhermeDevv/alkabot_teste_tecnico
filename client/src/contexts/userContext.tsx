201e780d91a9c1f1541b70345264a031278a87cd
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-21 15:46:34 -0300

    context of user

---
+++ client/src/contexts/userContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from "react";
+
+interface UserData {
+  name: string;
+  email: string;
+}
+
+interface AuthContextType {
+  userData: UserData;
+  setUserData: React.Dispatch<React.SetStateAction<UserData>>;
+  authorized: boolean;
+  setAuthorized: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  userData: { name: "", email: "" },
+  setUserData: () => {},
+  authorized: false,
+  setAuthorized: () => {},
+});
+
+export function ProvideUser({ children }) {
+  const [userData, setUserData] = useState<UserData>({ name: "", email: "" });
+  const [authorized, setAuthorized] = useState(false);
+
+  return (
+    <AuthContext.Provider
+      value={{ userData, setUserData, authorized, setAuthorized }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
