b63774e7ddf436ddddd6137fd7f9bed448c93420
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-26 14:54:47 -0300

    todaa aplicacao

--- server/src/app/repositories/UserRespository.ts
+++ server/src/app/repositories/UserRespository.ts
@@ -3,12 +3,10 @@ import UserInterface from "../types/user";
 
 class UserRepository {
     findAll(): void { }
-
     async find(email: string): Promise<UserInterface | null> {
         const user = await UserModel.findOne({ email });
         return user;
     }
-
     async findById(): Promise<void> { }
 
     async delete(id: string): Promise<void> { }
@@ -17,7 +15,6 @@ class UserRepository {
         const user = await UserModel.create(data);
         return user;
     }
-
     async update(filter: Partial<UserInterface>, data: Partial<UserInterface>): Promise<boolean> {
         await UserModel.findOneAndUpdate(filter, { $set: data }, { new: true });
         return true;
