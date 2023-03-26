9b6b9f0b032bd1d0b3e43b5167f0ee2514659200
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-26 15:14:20 -0300

    correcao de erro

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import UserController from "./app/controllers/userController";
+import UserController from "./app/controllers/UserController";
 
 const routes = Router();
 
