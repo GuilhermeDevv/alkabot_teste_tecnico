43cad346074bef6a1e16e40c87eccfa944aa8d63
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-29 19:21:41 -0300

    identecao no codigo

--- server/src/app/database/DB.ts
+++ server/src/app/database/DB.ts
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
-mongoose.set("strictQuery", false);
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', false);
 function connect() {
-    mongoose
-        .connect(process.env.MONGODB_URI)
-        .then(() => console.log("DB CONNECTED"))
-        .catch((error) => console.log(error));
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('DB CONNECTED'))
+    .catch((error) => console.log(error));
 }
 
 export default connect;
