99f3b9c0a1605129fbf0331a52f2f0f93012a480
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-29 19:21:21 -0300

    ajuste no nome da interface e identacao do codigo

--- server/src/app/models/userModel.ts
+++ server/src/app/models/userModel.ts
@@ -1,12 +1,13 @@
-import { Document, Schema, model } from "mongoose";
-import UserInterface from "../types/user";
+import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema<UserInterface>({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    resetPasswordToken: { type: String, default: "" },
-    resetPasswordExpires: { type: Date, default: new Date() }
+import IUserInterface from '../types/user';
+
+const userSchema = new Schema<IUserInterface>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordToken: { type: String, default: '' },
+  resetPasswordExpires: { type: Date, default: new Date() },
 });
 
-export default model<UserInterface>("user", userSchema);
+export default model<IUserInterface>('user', userSchema);
