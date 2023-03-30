478e467d164730e4e4fea325b555ace36fde88d7
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-29 23:06:20 -0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,16 @@ import routes from './routes';
 dotenv.config();
 connect();
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://alkabot.vercel.app');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
